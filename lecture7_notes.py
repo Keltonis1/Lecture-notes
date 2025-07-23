34b4067874f3007930161d070435ae6c3d357506
Kelson Cantrell <cantrellkelson@gmail.com>
2025-07-23 16:54:23 -0400

    added lec_7

---
+++ lecture7_notes.py
@@ -0,0 +1,19 @@
+####################################################
+# File I/O                                         #
+####################################################
+
+## Key Concepts:
+# - open(), read(), write(), close()
+# - Using `with open()` for context management
+
+
+
+## Code Demo:
+# with open("data.txt", "w") as f:
+#     f.write("Hello, file!")
+
+# with open("data.txt", "r") as f:
+#     print(f.read())
+
+## Exercises:
+# Write a program that appends user input to a file.
